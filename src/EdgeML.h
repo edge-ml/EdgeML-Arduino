4cb346b9c499af601b606cb2062b63ddae0e5e92
Xraydylan <xraydylan@gmail.com>
2022-05-28 06:36:22 +0200

    Found preprocessor define

--- src/EdgeML.h
+++ src/EdgeML.h
@@ -1,12 +1,10 @@
 #ifndef Edge_ML_BASE_H_
 #define Edge_ML_BASE_H_
 
-// can't find correct preprecessor condition for Seeed XIAO
-
 #if defined(ARDUINO_NICLA)
     #warning "Using Nicla Sense ME"
     #include<nicla/Edge_ML_Nicla.h>
-#elifdef TARGET_SEEED_XIAO_NRF52840_SENSE
+#elif defined(TARGET_SEEED_XIAO_NRF52840_SENSE)
     #warning "Using Seeed XIAO NRF52840 SENSE"
     #include<xiaoblesense/Edge_ML_Seeed.h>
 #else 
@@ -19,7 +17,7 @@ class Edge_ML {
         Edge_ML() {
           #if defined(ARDUINO_NICLA)
             Edge_ML_Nicla();
-          #elifdef TARGET_SEEED_XIAO_NRF52840_SENSE
+          #elif defined(TARGET_SEEED_XIAO_NRF52840_SENSE)
             Edge_ML_Seeed();
           #else 
             Edge_ML_Nano();
@@ -28,7 +26,7 @@ class Edge_ML {
         bool begin() {
           #if defined(ARDUINO_NICLA)
             edge_ml_nicla.begin();
-          #elifdef TARGET_SEEED_XIAO_NRF52840_SENSE
+          #elif defined(TARGET_SEEED_XIAO_NRF52840_SENSE)
             edge_ml_seeed.begin();
           #else 
             edge_ml_nano.begin();
@@ -37,7 +35,7 @@ class Edge_ML {
         void update() {
           #if defined(ARDUINO_NICLA)
             edge_ml_nicla.update();
-          #elifdef TARGET_SEEED_XIAO_NRF52840_SENSE
+          #elif defined(TARGET_SEEED_XIAO_NRF52840_SENSE)
             edge_ml_seeed.update();
           #else 
             edge_ml_nano.update();
@@ -46,7 +44,7 @@ class Edge_ML {
         void debug(Stream &stream) {
           #if defined(ARDUINO_NICLA)
             edge_ml_nicla.debug(stream);
-          #elifdef TARGET_SEEED_XIAO_NRF52840_SENSE
+          #elif defined(TARGET_SEEED_XIAO_NRF52840_SENSE)
             edge_ml_seeed.debug(stream);
           #else 
             edge_ml_nano.debug(stream);
