a57ab5a9123e7e4c3d35fbeab5bfadf2690623d0
Xray <xraydylan@gmail.com>
2023-03-31 21:00:23 +0200

    Sensor object creation in constructor

--- src/boards/generic_boards/ble33nano/SensorManager_Nano.cpp
+++ src/boards/generic_boards/ble33nano/SensorManager_Nano.cpp
@@ -1,13 +1,15 @@
 #include "SensorManager_Nano.h"
 
 
-IMU_Sensor_Nano imu_sensor;
-HTS_Sensor_Nano hts_sensor;
-BARO_Sensor_Nano baro_sensor;
-APDS_Sensor_Nano apds_sensor;
+
 
 SensorManager_Nano::SensorManager_Nano() {
-    SensorInterface ** sensors = new SensorInterface * [MODULE_COUNT_PHYSICAL_NANO] {(&imu_sensor),(&hts_sensor),(&baro_sensor),(&apds_sensor)};
+    IMU_Sensor_Nano * imu_sensor = new IMU_Sensor_Nano();
+    HTS_Sensor_Nano * hts_sensor = new HTS_Sensor_Nano();
+    BARO_Sensor_Nano * baro_sensor = new BARO_Sensor_Nano();
+    APDS_Sensor_Nano * apds_sensor = new APDS_Sensor_Nano();
+
+    SensorInterface ** sensors = new SensorInterface * [MODULE_COUNT_PHYSICAL_NANO] {(imu_sensor),(hts_sensor),(baro_sensor),(apds_sensor)};
 
     SensorManagerInterface::set_sensors(sensors);
     SensorManagerInterface::set_sensor_counts(SENSOR_COUNT_NANO, MODULE_COUNT_PHYSICAL_NANO);
